717070608c28d74349e6588e50a104d79e235441
Ajinkya Devasthali <ajex13@gmail.com>
2020-02-18 23:55:12 +0530

    Readme updated code cleanup restructuring redux react-redux redux-thunk configuration done

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import { ADD_POST, DELETE_POST, FETCH_POST } from './types';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:4000/';
+
+// Add task
+// Fetch tasks
+// Complete task
+// Delete task
---
+++ src/actions/types.js
@@ -0,0 +1,8 @@
+// Add task
+export const ADD_TASK = 'ADD_TASK';
+// Fetch tasks
+export const FETCH_TASK = 'FETCH_TASK';
+// Complete task
+export const COMPLETE_TASK = 'COMPLETE_TASK';
+// Delete task
+export const DELETE_TASK = 'DELETE_TASK';
